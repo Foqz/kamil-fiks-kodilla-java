38d8bf274ec74b1a9b64752c4242599aae230b07
Foqz <fikskamil@gmail.com>
2018-08-09 15:34:05 +0200

    10.3

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardTestSuite {
+    @Test
+    public void testTaskAdd(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
+        Board board = context.getBean(Board.class);
+        //When & Then
+        System.out.println(board.getToDoList());
+        System.out.println(board.getInProgressList());
+        System.out.println(board.getDoneList());
+    }
+}
