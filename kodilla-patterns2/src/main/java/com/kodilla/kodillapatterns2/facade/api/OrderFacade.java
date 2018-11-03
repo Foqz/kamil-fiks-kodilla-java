2aa49d58c33248c3f5ed5354122b10e452ac8431
Foqz <fikskamil@gmail.com>
2018-11-03 14:46:49 +0100

    20.3 Spring-AOP

--- kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/kodillapatterns2/facade/api/OrderFacade.java
@@ -3,12 +3,14 @@ package com.kodilla.kodillapatterns2.facade.api;
 import com.kodilla.kodillapatterns2.facade.ShopService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class OrderFacade {
+    @Autowired
     private ShopService shopService;
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 
--- kodilla-patterns2/src/test/java/com/kodilla/kodillapatterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/kodillapatterns2/facade/ShopServiceTestSuite.java
@@ -8,12 +8,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
